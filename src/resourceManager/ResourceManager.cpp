9775ea5e8621d55fbd7b66f464bbb9a06e9f076f
VLAD <vledbress388@gmail.com>
2025-10-28 16:08:25 +0100

    refactored rm

--- src/resourceManager/ResourceManager.cpp
+++ src/resourceManager/ResourceManager.cpp
@@ -1,80 +1,26 @@
 #include "ResourceManager.h"
+#include <iostream>
 
-
-ResourceManager::ResourceManager(const std::string& pathToRes)
+void ResourceManager::add_resource(const std::string& id, std::shared_ptr<IResource> new_resource)
 {
-	int index = pathToRes.find_last_of("\\");
-	resPath = pathToRes.substr(0, index) + '\\' + "res\\";
+    resource_map[id] = std::move(new_resource);
 }
 
-ptrToProg ResourceManager::addProgram(const std::string& name, const std::string& vertexPath, const std::string& fragmentPath)
+bool ResourceManager::exist(const std::string& id)
 {
-	std::string vertexSource = getFileString(vertexPath);
-	std::string fragmentSource = getFileString(fragmentPath);
-
-	ptrToProg newShaderProgram = std::make_shared<ShaderProgram>(vertexSource, fragmentSource);
-
-	if (!newShaderProgram->compiled()) {
-		std::cout << "Program not compiled\n";
-		return nullptr;
-	}
-
-	shaderMap.emplace(name, newShaderProgram);
-
-	return newShaderProgram;
+    return resource_map.find(id) != resource_map.end();
 }
 
-
-ptrToProg ResourceManager::getProgram(const std::string& name)
+std::shared_ptr<IResource> ResourceManager::get_resource(const std::string& id)
 {
-	auto it = shaderMap.find(name);
-	if (it != shaderMap.end()) {
-		return it->second; 
-	}
-	return nullptr; 
+    auto it = resource_map.find(id);
+    if (it != resource_map.end()) {
+        return it->second;
+    }
+    return nullptr;
 }
 
-ptrToTO ResourceManager::addTexture(const std::string& name, const std::string& path)
+void ResourceManager::remove(const std::string& id)
 {
-	return nullptr;
+    resource_map.erase(id);
 }
-
-ptrToTO ResourceManager::getTexture(const std::string& name)
-{
-	auto it = textureMap.find(name);
-	if (it != textureMap.end()) {
-		return it->second;
-	}
-	return nullptr;
-}
-
-void ResourceManager::remove(std::string& mesh_id)
-{
-}
-
-
-
-
-
-
-std::string ResourceManager::getFileString(const std::string& relatedPath)
-{
-	std::string fullPath = resPath + relatedPath;
-	std::ifstream file(fullPath);
-
-	if (!file.is_open()) {
-		throw std::runtime_error("Failed to open file: " + fullPath);
-	}
-
-	std::ostringstream buffer;
-	buffer << file.rdbuf(); 
-	return buffer.str();    
-}
-
-unsigned char* ResourceManager::loadTexture(const std::string& relatedPath, int* width, int* height, int* channels)
-{
-	return nullptr;
-}
-
-
-
--- src/resourceManager/ResourceManager.h
+++ src/resourceManager/ResourceManager.h
@@ -1,41 +1,17 @@
 #pragma once
 #include <string>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
 #include <memory>
 #include <unordered_map>
-#include <fstream>
-#include <sstream>
-#include "../renderer/ShaderProgram.h"
-#include "../geometry/Geometry.h"
-#include "../textureObject/TextureObject.h"
-
-typedef std::shared_ptr<ShaderProgram> ptrToProg;
-typedef std::unordered_map<std::string, ptrToProg> progMap;
-typedef std::unordered_map<std::string, ptrToTO> textMap;
-
-
+#include "../resource/Resource.h"
 
 class ResourceManager {
 public:
-	ResourceManager(const std::string& pathToRes);
-
-	ptrToProg addProgram(const std::string& name, const std::string& vertexPath, const std::string& fragmentPath);
-	ptrToProg getProgram(const std::string& name);
-
-	ptrToTO addTexture(const std::string& name, const std::string& path);
-	ptrToTO getTexture(const std::string& name);
-
-	void remove(std::string& mesh_id);
+	void add_resource(const std::string& id, std::shared_ptr<IResource> new_resource);
+	bool exist(const std::string& id);
+	std::shared_ptr<IResource> get_resource(const std::string& id);
+	void remove(const std::string& id);
 private:
-	std::string resPath;
-	progMap shaderMap;
-	textMap textureMap;
-
-
-	std::string getFileString(const std::string& relatedPath);
-	unsigned char* loadTexture(const std::string& relatedPath, int* width, int* height, int* channels);
+	std::unordered_map<std::string, std::shared_ptr<IResource>> resource_map;
 };
 
 
